3f5218bb0747ebf2daa6c0bd2abf7b94396d8201
Andrii Borychevskyi <andrii.borychevskyi@signus.ai>
2025-09-15 16:27:48 +0300

    add: ml process endpoint

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from api.v1.endpoints.assets import assets_router
+from api.v1.endpoints.ml import ml_router
 from api.v1.endpoints.system import system_router
 
 v1_router = APIRouter()
 v1_router.include_router(system_router, prefix="/system", tags=["system"])
 v1_router.include_router(assets_router, prefix="/assets", tags=["assets"])
+v1_router.include_router(ml_router, prefix="/ml", tags=["ml"])
--- src/api/v1/endpoints/ml/__init__.py
+++ src/api/v1/endpoints/ml/__init__.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
 
+from .process import process_router
 
 ml_router = APIRouter()
+ml_router.include_router(process_router, prefix="/process", tags=["ml"])
